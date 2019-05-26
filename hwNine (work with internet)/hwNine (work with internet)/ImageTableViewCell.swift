6fc27dd4a76dc449bb5c53114c5a7f3f9de4c361
Tatsiana Pasiukova <tatiana.will.read@gmail.com>
2019-05-27 01:39:17 +0300

    home work 9th with comments

--- hwNine (work with internet)/hwNine (work with internet)/ImageTableViewCell.swift
+++ hwNine (work with internet)/hwNine (work with internet)/ImageTableViewCell.swift
@@ -1,19 +1,18 @@
 import UIKit
 
 class ImageTableViewCell: UITableViewCell {
-    
+//7
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var myLabel: UILabel!
 
-//20
+//20 создаем ф-ию для заполнения ячейки
     func setUpCell(imageInfo: ImageInfo) {
         myLabel.text = imageInfo.title
-        //24
+//24 вызываем ф-ию loadImage(url)
         loadImage(url: imageInfo.url)
-        
     }
 
-//23
+//23 создаем ф-ию для загрузки картинки по url
     func loadImage(url: URL) {
         let dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else { return }
--- hwNine (work with internet)/hwNine (work with internet)/ViewController.swift
+++ hwNine (work with internet)/hwNine (work with internet)/ViewController.swift
@@ -1,57 +1,58 @@
 import UIKit
-//14
+
+//14 создаем структуру для данных
 struct ImageInfo {
     let url: URL
     let title: String
 }
+
 class ViewController: UIViewController {
-    //17
+//17
     @IBOutlet weak var myTable: UITableView!
     
-//8
+//8 создаем переменную с адресом сайта
     let urlString = "http://www.raywenderlich.com/downloads/ClassicPhotosDictionary.plist" //url по которому будем делать запрос
-    //15
+    
+//15 создаем массив эллементов ImageInfo
     var imageInfoArray: [ImageInfo] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         downloadData()
-        
-        
     }
-  //10
+    
+//10 загружаем данные в оперативную память (func downloadData)
     func downloadData() {
-        guard let url = URL(string: urlString) else { return }
-        //получаем url
-        
-    //11
+        guard let url = URL(string: urlString) else { return }  //получаем url
+       
+//11 создаем переменную для загрузки данных
         let dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
-//            print("URL: \(url)") //место куда загрузился файл
-//            print("response: \(response)") //объект кот сод метаданные ответа
-//            print("error: \(error)") //ошибка кот произошла или нет
-     //13
+      //      print("URL: \(url)") //место куда загрузился файл
+      //      print("response: \(response)") //объект кот сод метаданные ответа
+      //      print("error: \(error)") //ошибка кот произошла или нет
+            
+//13 вызываем метод parse(data)
             guard let data = data else { return } //разворачиваем значение data, чтобы не использовать опциональное значение
             self.parse(data: data)
-      //22
+            
+//22 перезагружаем таблицу в главном потоке, чтобы она подтянула данные из массива
             DispatchQueue.main.async {
                  self.myTable.reloadData()
             }
         }
         dataTask.resume() // нужно вызвать resume, чтобы загрузка началась, тк по умолчанию задачи на загрузку приостановлены
     }
-    //загружаем данные в оперативную память (func downloadData)
 
-    //12
+//12 парсим данные (func parse)
     func parse(data: Data) {
         do {
             let animalsDictionary = try PropertyListDecoder().decode(Dictionary<String, String>.self, from: data) //парсим данные с помощью PropertyListDecoder в структуру Dictionary
             
-    //16
+//16 преобразовываем словарь в массив
             for (key, value) in animalsDictionary {
-                guard let url = URL(string: value) else { continue }
+                guard let url = URL(string: value) else { continue } // не return, тк return возвращается к вызову функции. Инструкция continue заканчивает выполнение программы текущей итерации инструкции цикла, но не останавливает выполнение инструкции цикла
                 let imageInfo = ImageInfo(url: url, title: key)
                 imageInfoArray.append(imageInfo)
-                
             }
             
         } catch let error {
@@ -60,7 +61,7 @@ class ViewController: UIViewController {
       // do- catch для отлавливания ошибок
     }
 }
-//19
+//19 extension для таблицы
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
        return imageInfoArray.count
@@ -68,11 +69,10 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ImageTableViewCell", for: indexPath) as? ImageTableViewCell else { return UITableViewCell()} //выгружаем переиспользованную ячейку, преобразуем ее тип в целевой тип (ImageTableViewCell), чтобы заполнить ее данными
- //21
+        
+ //21 заполняем ячейку данными из массива
         cell.setUpCell(imageInfo: imageInfoArray[indexPath.row])
         return cell
         
     }
-    
-    
 }
