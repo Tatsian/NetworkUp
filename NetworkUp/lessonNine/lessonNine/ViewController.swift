cbad5f3f9aef35282f1d9b45dcfcebde5fbc01a4
Tatsiana Pasiukova <tatiana.will.read@gmail.com>
2019-05-26 14:39:55 +0300

    network project

---
+++ NetworkUp/lessonNine/lessonNine/ViewController.swift
@@ -0,0 +1,61 @@
+
+import UIKit
+
+struct User: Decodable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let completed: Bool
+}
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textLabal: UILabel!
+    let urlString = "https://jsonplaceholder.typicode.com/todos/4"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func actionButton(_ sender: Any) {
+        let url = URL(string: urlString)
+        let downloadTask = URLSession.shared.downloadTask(with: url!) { (url, response, error) in
+            
+//            print("URL: \(url)") //место куда загрузился файл
+//            print("response: \(response)") //объект кот сод метаданные ответа
+//            print("error: \(error)") //ошибка кот произошла или нет
+        }
+        downloadTask.resume()
+        
+        let dataTask = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            let user = try! JSONDecoder().decode(User.self, from: data!)
+            let dictionaty = try! JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String:AnyObject]
+            let title = dictionaty["title"]
+            print("Data: \(data)")
+            print(String(data: data!, encoding: .utf8))
+            
+            DispatchQueue.main.async {
+                self.updateUser(user: user)
+            }
+        }
+        dataTask.resume()
+        
+    }
+    
+    func updateUser(user: User) {
+        textLabal.text = user.title
+        
+        if user.completed {
+            textLabal.textColor = .green
+        } else {
+            textLabal.textColor = .red
+        }
+        
+        
+    }
+    
+    
+
+}
+
